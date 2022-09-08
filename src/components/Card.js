d5bdc541abfd721a65b68cbe43b6c77bfbf56346
Grace DIta Hutasoit <74285510+gracedita1@users.noreply.github.com>
2022-09-08 11:53:41 +0700

    update

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,24 +1,21 @@
-import React from 'react'
-import { Col, Image, Card } from 'react-bootstrap';
-
+import React from "react";
+import { Col, Image, Card } from "react-bootstrap";
 
 export default function CardMovie(props) {
   return (
-      <Col md={4} className="movieWrapper" >
-        <Card className="movieImage">
-          <Image src={props.image} alt="Jurassic Movie" className="images" />
-          <div className="bg-dark">
-            <div className="p-2 m-1 text-white">
-              <Card.Title className="text-center">{props.title}</Card.Title>
-              <Card.Text className="text-left">
-                {props.description}
-              </Card.Text>
-              <Card.Text className="text-left">
-                Last updated 3 mins ago
-              </Card.Text>
-            </div>
+    <Col md={4} className="movieWrapper">
+      <Card className="movieImage">
+        <Image src={props.image} alt="Jurassic Movie" className="images" />
+        <div className="bg-dark">
+          <div className="p-2 m-1 text-white">
+            <Card.Title className="text-center">{props.title}</Card.Title>
+            <Card.Text className="text-left description">
+              {props.description}
+            </Card.Text>
+            <Card.Text className="text-left">Last updated 3 mins ago</Card.Text>
           </div>
-        </Card>
-      </Col>
+        </div>
+      </Card>
+    </Col>
   );
 }
